c836c41df28249ae5fd435da5429b2ee21164200
Andras Mitzki <mitzkia@gmail.com>
2015-07-07 07:07:11 +0200

    Possible clone custom syslog-ng repository.
    with:
    After building basic-buil-env, and developement-images, now we can use:
    docker run -e BRANCH_NAME=[branch-name] -e BRANCH_URL=[branch-repo-url] balabit/debian_syslog_ng_dev
    
    Signed-off-by: Andras Mitzki <mitzkia@gmail.com>

--- Dockerfiles/basic-build-environments/debian_jessie/checkout_and_build_syslog_ng.sh
+++ Dockerfiles/basic-build-environments/debian_jessie/checkout_and_build_syslog_ng.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-git clone -b ${BRANCH_NAME} https://github.com/balabit/syslog-ng.git /syslog-ng-src
+git clone -b ${BRANCH_NAME} ${BRANCH_URL} /syslog-ng-src
 cd /syslog-ng-src
 git pull
 ./autogen.sh
@@ -18,3 +18,19 @@ mkdir build_dir && cd build_dir && mkdir -p ${INSTALL_DIR}
 
 make
 make install
+
+if [ ${BRANCH_NAME}=master ] ; then
+  git clone -b ${INCUBATOR_BRANCH_NAME} https://github.com/balabit/syslog-ng-incubator.git /syslog-ng-incubator-src
+  cd /syslog-ng-incubator-src
+  git pull
+  autoreconf -i
+  mkdir build_dir && cd build_dir && mkdir -p ${INSTALL_DIR}
+  ../configure \
+    --prefix=${INSTALL_DIR}
+  make
+  make install
+else
+  echo "syslog-ng OSE incubator not supported with this OSE version: ${BRANCH_NAME}, it will be not downloaded, installed."
+fi
+
+/opt/syslog-ng/sbin/syslog-ng -Fedv
