a2795996203ff3af1eadcc6bc23f14d5b2820160
TheCase <github@repulsor.net>
2018-01-25 17:56:12 -0700

    connection tuning

--- syslog-ng-es/docker-entrypoint.sh
+++ syslog-ng-es/docker-entrypoint.sh
@@ -5,10 +5,12 @@
 export LD_LIBRARY_PATH=/usr/lib/jvm/java-1.8-openjdk/jre/lib/amd64/server:$LD_LIBRARY_PATH
 
 MAX_CONNECTIONS=${MAX_CONNECTIONS:=10}
+LOG_IW_SIZE=$(expr $MAX_CONNECTIONS \* 100)
 
 sed -i "s/ES_SERVER/$ES_SERVER/g" /etc/syslog-ng/syslog-ng.conf
 sed -i "s/ES_CLUSTER/$ES_CLUSTER/g" /etc/syslog-ng/syslog-ng.conf
 sed -i "s/MAX_CONNECTIONS/$MAX_CONNECTIONS/g" /etc/syslog-ng/syslog-ng.conf
+sed -i "s/LOG_IW_SIZE/$LOG_IW_SIZE/g" /etc/syslog-ng/syslog-ng.conf
 
 if [ -z "$RUNTIME_ARGS" ]; then
   export RUNTIME_ARGS="F"
